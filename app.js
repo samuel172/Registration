758015b157c0e089769d390b650da6facbc4db86
samuel172 <elliott.sam.j@gmail.com>
2021-06-19 15:24:01 +1000

    started to add validation.

--- app.js
+++ app.js
@@ -4,15 +4,26 @@ const app = Vue.createApp({
             moistTacos: 'dogs',
             prompt: true,
             extraGuests: 0,
+            errors: [],
+            fName1: null,
+            sName1: null,
+            guestEmail1: null,
+            guestPhone: null,
+            dietary1: null,
+            fName2: null,
+            sName2: null,
+            guestEmail2: null,
+            dietary2: null,
+            fName3: null,
+            sName3: null,
+            guestEmail3: null,
+            dietary3: null,
         };
     },
     methods: {
         extraGuest() {
             this.extraGuests += 1;
             this.prompt = false;
-            console.log('yes this worked');
-            console.log(this.extraGuests);
-            console.log(this.prompt);
         }
     }
 });
